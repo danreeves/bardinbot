6ac2cfda6db0262394825864d3f0142eb55fbdb9
Dan Reeves <hey@danreev.es>
2020-02-16 16:10:54 -0800

    add first responder

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").env();
+require("dotenv").config();
 const HelperBot = require("@danreeves/helper-bot");
 const DontReplyToSelf = require("@danreeves/helper-bot/middleware/dont-reply-to-self");
 const Responder = require("@danreeves/helper-bot/middleware/responder");
@@ -9,8 +9,8 @@ helper
   .use(new DontReplyToSelf())
   .use(
     new Responder({
-      command: /do you know what i think/gi,
-      response: "Do I know what elf thinks? No one does!",
+      command: /know what i think/gi,
+      response: "do I know what an elf thinks? No one does!",
     }),
   )
   .start();
