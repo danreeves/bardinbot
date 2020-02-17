ea38ce34ccb08369ad397b820b801731e12958d0
Dan Reeves <hey@danreev.es>
2020-02-16 18:23:38 -0800

    try notifying the user

--- role-reactions.js
+++ role-reactions.js
@@ -18,28 +18,46 @@ module.exports = class RoleReactions {
     channels.forEach(channel => channel.fetchMessages({ limit: 20 }));
   }
 
-  reactionAdd(reaction, user) {
+  async reply(user, roleName, given) {
+    let dm;
+
+    if (user.dmChannel) {
+      dm = user.dmChannel;
+    } else {
+      dm = await user.createDM();
+    }
+
+    if (given) {
+      dm.send(`Oi, I've given you the ${roleName} role.`);
+    } else {
+      dm.send(`Oi, I've removed the ${roleName} role from you.`);
+    }
+  }
+
+  async reactionAdd(reaction, user) {
     if (this.messages.includes(reaction.message.id)) {
       const roleName = this.mapping[reaction.emoji.id];
       if (roleName) {
         const guild = reaction.message.guild;
         const member = guild.member(user);
         const role = guild.roles.array().find(role => role.name === roleName);
-        member.addRole(role);
+        await member.addRole(role);
         console.log(`Adding role ${roleName} to ${user.username}`);
+        this.reply(user, roleName, true);
       }
     }
   }
 
-  reactionRemove(reaction, user) {
+  async reactionRemove(reaction, user) {
     if (this.messages.includes(reaction.message.id)) {
       const roleName = this.mapping[reaction.emoji.id];
       if (roleName) {
         const guild = reaction.message.guild;
         const member = guild.member(user);
         const role = guild.roles.array().find(role => role.name === roleName);
-        member.removeRole(role);
+        await member.removeRole(role);
         console.log(`Removing role ${roleName} to ${user.username}`);
+        this.reply(user, roleName, false);
       }
     }
   }
