2501e1ac24ef6663e11f8eac58a8316e0ca802ba
Dan Reeves <hey@danreev.es>
2020-02-21 18:27:37 -0800

    does this work

--- bad-words.js
+++ bad-words.js
@@ -3,25 +3,33 @@ const badwordsContent = readFileSync("./bad-words.txt", "utf8");
 const badwords = badwordsContent
   .split("\n")
   .map(str => str.trim())
-  .filter(str => str.length > 0);
-
-console.log(badwords);
+  .filter(str => str.length > 0)
+  .map(word => new RegExp(`(^|\\W)${word}($|\\W)`, "gi"));
 
 module.exports = class BadWords {
   message(message, _state, kill) {
-    const matches = badwords.filter(word => message.content.includes(word));
+    const matches = badwords.filter(matcher => {
+      const matches = message.content.match(matcher);
+      return matches !== null && matches.length > 0;
+    });
+
+    matches.sort((a, b) => {
+      return a.length > b.length ? -1 : 1;
+    });
+
     if (matches.length > 0) {
       kill(); // Don't do anything after this
 
       const newContents = matches.reduce((str, match) => {
-        console.log("replacing", match);
         return str.replace(match, "umgak");
       }, message.content);
 
-      console.log(newContents);
-
-      // M message.reply(`you said: ${newContents}`);
-      // message.delete();
+      if (message.deletable) {
+        message.reply(`you said: ${newContents}`);
+        message.delete();
+      } else {
+        message.reply("watch your mouth, umgi");
+      }
     }
   }
 };
