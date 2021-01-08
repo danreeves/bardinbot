44c550b6722a65cebc858e9e57de8040a98f9964
Dan Reeves <hey@danreev.es>
2021-01-08 01:58:48 +0000

    msg instead of reply

--- weekly.js
+++ weekly.js
@@ -3,11 +3,11 @@ const fetch = require("node-fetch");
 class Weekly {
 	message(msg) {
 		if (msg.content.startsWith("/weekly")) {
-			msg.reply("Let me check...");
+			msg.channel.send("Let me check...");
 			fetch("https://weekly.verminti.de")
 				.then((res) => res.text())
 				.then((text) => {
-					msg.reply(text);
+					msg.channel.send(text);
 				});
 		}
 	}
